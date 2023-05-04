3c4a502c1bde47ef0618d3b0b4d508e06809cedf
Alexia Kattah <alexiakattah@gmail.com>
2023-05-04 09:50:28 -0300

    first commit

---
+++ src/components/UsersList/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface User {
+  id: string;
+  avatar_url: string;
+  login: string;
+}
+
+interface Props {
+  users: User[];
+}
+
+const UsersList: React.FC<Props> = ({ users }) => (
+  <>
+    {users.map((user, index) => {
+      return (
+        <div key={index}>
+          <img src={user.avatar_url} alt="" />
+          <span>{user.login}</span>
+        </div>
+      );
+    })}
+  </>
+);
+
+export default UsersList;
